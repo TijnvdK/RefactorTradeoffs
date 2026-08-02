735c8e7fde609800127f31a14174cf92244eaf9d
ThijsJ04 <tijnvdk@gmail.com>
2026-08-02 12:07:10 +0200

    feat: Add active/agent results + Result parsing notebooks

---
+++ src/result_analysis/plot_style.py
@@ -0,0 +1,16 @@
+FONTS = {
+    'font.family': 'serif',
+    'font.serif': ['Crimson Pro'],
+    'mathtext.fontset': 'custom',
+    'mathtext.rm': 'Crimson Pro',
+    'mathtext.bf': 'Crimson Pro:bold',
+    'mathtext.it': 'Crimson Pro:italic',
+    'font.size': 12,
+}
+
+COLOR_PALLETTE = {
+    'blue': '#0072B2',
+    'orange': '#D55E00',
+    'green': '#009E73',
+    'yellow': '#E69F00',
+}
